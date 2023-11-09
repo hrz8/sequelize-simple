815b2507612c00cbe40d9b3deca29d4644289ca3
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-11-09 21:39:22 +0700

    redacted

--- db/config.js
+++ db/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "freedb_utodolist_skv",
-    password: "5Ae9zW!YG?%TdGU",
-    database: "freedb_todolist_db",
-    host: "sql.freedb.tech",
+    username: "<REDACTED>",
+    password: "<REDACTED>",
+    database: "<REDACTED>",
+    host: "<REDACTED>",
     port: 3306,
     dialect: "mysql",
   }
