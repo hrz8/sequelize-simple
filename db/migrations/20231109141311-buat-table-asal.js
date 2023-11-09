ffa67e387b86a01b8b89d0ae079cef94837c27ae
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-11-09 21:16:53 +0700

    tambah more migration

---
+++ db/migrations/20231109141311-buat-table-asal.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('asal', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      is_completed: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('asal');
+  }
+};
